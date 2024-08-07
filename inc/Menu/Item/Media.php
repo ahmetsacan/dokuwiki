cc847e36aee2f73411070915092ae9d3c1b73dbf
ahmetsacan <ahmetdevel@umich.edu>
2024-08-07 12:11:30 -0400

    show mediamanager only if user has edit rights to page or expose rights to namespace.

--- inc/Menu/Item/Media.php
+++ inc/Menu/Item/Media.php
@@ -15,6 +15,8 @@ class Media extends AbstractItem
         global $ID;
         parent::__construct();
 
+        if (auth_quickaclcheck(getNS($ID).':*') < AUTH_EXPOSE && auth_quickaclcheck($ID) < AUTH_EDIT) throw new \RuntimeException("no permission to see the media manager");
+
         $this->svg = DOKU_INC . 'lib/images/menu/folder-multiple-image.svg';
         $this->params['ns'] = getNS($ID);
     }
